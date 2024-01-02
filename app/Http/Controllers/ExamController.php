235d3b40e27e283b57ef1576a6e352f333cd6431
Prashant Kumar <prashant4sengar96@gmail.com>
2024-01-02 12:08:01 +0530

    creat question api fuction

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ExamController extends Controller
 {
-    //
+    public function addQuestion(Request $request){
+       return 'ok';
+    }
+
+    public function editQuestion(Request $request){
+        return 'ok';
+    }
+
+
+    public function deleteQuestion(Request $request){
+        return 'ok';
+    }
+
+   
 }
---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    public function addQuestion(Request $request){
+        return 'ok';
+     }
+ 
+     public function editQuestion(Request $request){
+         return 'ok';
+     }
+ 
+ 
+     public function deleteQuestion(Request $request){
+         return 'ok';
+     }
+}
